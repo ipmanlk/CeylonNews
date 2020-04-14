631c41a826066da319fee49e10bae093a1194600
ipmanlk <io@navinda.xyz>
2020-04-15 02:23:54 +0530

    add: api token header to sendRequest

--- www/js/main.js
+++ www/js/main.js
@@ -512,14 +512,21 @@ const sendRequest = (data = {}, method = "get") => {
         $.ajax({
             url: "https://s1.navinda.xyz/ceylon_news/v2.0/",
             method: method,
+            beforeSend: (request) => {
+                request.setRequestHeader("token", "token");
+            },
             dataType: "json",
             data: data,
             timeout: 10000
         }).done((res) => {
             resolve(res);
-        }).fail(() => {
+        }).fail((jqXHR) => {
             hideOutputToast();
-            showTimedToast("Request failed!.", 3000);
+            if (jqXHR.responseJSON.error) {
+                showTimedToast(jqXHR.responseJSON.error, 3000);
+            } else {
+                showTimedToast("Request failed!.", 3000);
+            }
             reject("Request failed.");
         })
     });
