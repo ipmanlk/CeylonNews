91c8cb92343f40176957333cf4bcddd6e77c05d1
ipmanlk <io@navinda.xyz>
2020-04-16 19:44:47 +0530

    change: moved sendRequest() to a seperate file

--- www/js/main.js
+++ www/js/main.js
@@ -506,28 +506,3 @@ const showDisclaimer = () => {
         exitApp();
     }
 }
-
-const sendRequest = (data = {}, method = "get") => {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: "https://s1.navinda.xyz/ceylon_news/v2.0/",
-            method: method,
-            beforeSend: (request) => {
-                request.setRequestHeader("token", "token");
-            },
-            dataType: "json",
-            data: data,
-            timeout: 10000
-        }).done((res) => {
-            resolve(res);
-        }).fail((jqXHR) => {
-            hideOutputToast();
-            if (jqXHR.responseJSON.error) {
-                showTimedToast(jqXHR.responseJSON.error, 3000);
-            } else {
-                showTimedToast("Request failed!.", 3000);
-            }
-            reject("Request failed.");
-        })
-    });
-}
